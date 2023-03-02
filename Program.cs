14e091f66fae4ac7a7dd51e1dbeb149c17a5a5af
ReTims <renatatims@gmail.com>
2023-03-02 13:37:57 -0600

    Created new console app with dotnet + gitignore + basic app testing on Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace idCard.Corporate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing application");
+        }
+    }
+}

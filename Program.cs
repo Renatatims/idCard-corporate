1bfb4ea9abe8119d36acd6c0f7c6d0bf811073f1
ReTims <renatatims@gmail.com>
2023-03-02 16:13:03 -0600

    Program.cs - Added Get Employees Method

--- Program.cs
+++ Program.cs
@@ -4,9 +4,27 @@ namespace idCard.Corporate
 {
     class Program
     {
+        //Method - Get Employees
+        static List<string> GetEmployees()
+        {
+            List<string> employees = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                string input = Console.ReadLine() ?? "";
+                // Breaks if user doesn't input data
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
+            }
+            return employees;
+        }
+        // Create - Print employees method
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing application");
+            List<string> employees = GetEmployees();
         }
     }
 }

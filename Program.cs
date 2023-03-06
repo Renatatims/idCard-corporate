6e95151d03aa7b9cbb589fa49b8b44c7fed09e0e
ReTims <renatatims@gmail.com>
2023-03-06 15:45:20 -0600

    Included an initial prompt - user has 2 options to generate the idCards: manual data input or get information from the API and 10 random idCards will be generated

--- Program.cs
+++ Program.cs
@@ -9,21 +9,44 @@ namespace idCard.Corporate
         async static Task Main(string[] args)
         {
             List<Employee> employees;
+            Console.WriteLine("Please select an option to generate the idCards:");
 
-            /* GetEmployees method call
-            employees = EmployeeFetcher.GetEmployees();*/
+            Console.WriteLine("\t1 - Manually input employee's data");
+            Console.WriteLine("\t2 - Get data from an API - 10 random idCards will be generated");
 
-            //GetFromApi method call
-            employees = await EmployeeFetcher.GetFromApi();
+            // Switch satatement
+            switch (Console.ReadLine() ?? "")
+            {
+                case "1":
+                    // GetEmployees method call
+                    employees = EmployeeFetcher.GetEmployees();
 
-            // Print employees method
-            Util.PrintEmployees(employees);
+                    // Print employees method
+                    Util.PrintEmployees(employees);
 
-            // MakeCSV - method call
-            Util.MakeCSV(employees);
+                    // MakeCSV - method call
+                    Util.MakeCSV(employees);
 
-            //MakeBadges - method call
-            await Util.MakeIdCards(employees);
+                    //MakeBadges - method call
+                    await Util.MakeIdCards(employees);
+
+                    break;
+
+                case "2":
+                    //GetFromApi method call
+                    employees = await EmployeeFetcher.GetFromApi();
+
+                    // Print employees method
+                    Util.PrintEmployees(employees);
+
+                    // MakeCSV - method call
+                    Util.MakeCSV(employees);
+
+                    //MakeBadges - method call
+                    await Util.MakeIdCards(employees);
+
+                    break;
+            }
         }
     }
 }
